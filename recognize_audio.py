dd931b5f72e8954453f66266404e6b4e7ebdbda1
ErnestoAragon03 <ernesto.aragon@galileo.edu>
2024-09-11 10:13:57 -0600

    Mejorado el funcionamiento de recognizad_audio.py, ahora se mantiene activo hasta detenerlo de manera manual.  Guarda el resultado en una variable global llamada recognized_text

--- recognize_audio.py
+++ recognize_audio.py
@@ -5,27 +5,42 @@ import json
 import sys  # Importa la librería sys
 
 # Configura el modelo y el reconocimiento
-#laptop_DELL = "C:\User\ernes\OneDrive\Documentos\Proyecto ARTEMISA\env\ASR\Local\Model\vosk-model-small-es-0.42"
-#desktop = "C:\Users\Ernesto\OneDrive\Documentos\Proyecto ARTEMISA\env\ASR\Local\Model\vosk-model-small-es-0.42"
+#laptop_DELL = "C:\User\ernes\OneDrive\Documentos\Proyecto ARTEMISA\ASR\Local\Model\vosk-model-small-es-0.42"
+#desktop = "C:\Users\Ernesto\OneDrive\Documentos\Proyecto ARTEMISA\ASR\Local\Model\vosk-model-small-es-0.42"
 
-model_path = r"C:\Users\Ernesto\OneDrive\Documentos\Proyecto ARTEMISA\env\ASR\Local\Model\vosk-model-small-es-0.42"
+#Path hacia la ubicación del modelo Vosk (Modificar más adelante para evitar problemas de compatibilidad)
+model_path = r"C:\Users\Ernesto\OneDrive\Documentos\Proyecto ARTEMISA\ASR\Local\Model\vosk-model-small-es-0.42"
+samplerata = 16000  #Frecuencia de muestreo (Depende del micrófono)
+blocksize = 8000     #Tamaño de los bloques de procesamiento
+dtype = 'int16'     #Formato de los datos de audio
+#Incializar el modelo Vosk
 model = vosk.Model(model_path)
-rec = vosk.KaldiRecognizer(model, 16000)
+rec = vosk.KaldiRecognizer(model, samplerata)
+
+#Variable donde almacenar el texto transcrito por Vosk
+recognized_text =""
+
+
 
 print(sd.query_devices())
 # Función de callback para el flujo de audio
 def callback(indata, frames, time, status):
+    global recognized_text
     if status:
-        print(status, file=sys.stderr)
+        print(status, flush=True, file=sys.stderr)
+    
     if rec.AcceptWaveform(indata.tobytes()):
         result = json.loads(rec.Result())
+        recognized_text = result.get('text', '')
         print("Texto reconocido:", result.get('text', ''))
     else:
         print(rec.PartialResult())
+
 #Selecciona el dispositivo de entrada (device) Si no se especifica usa el default que tiene el sistema
 #device_id = 10
 
-# Configuración del flujo de audio
-with sd.InputStream(samplerate=16000, blocksize = 8000, dtype="int16", channels=1, callback=callback):
-    print("Iniciando reconocimiento de voz...")
-    sd.sleep(10000)  # Mantén el script en ejecución
+# Iniciar la captura de audio
+with sd.InputStream(samplerate=samplerata, blocksize = blocksize, dtype=dtype, channels=1, callback=callback):
+    print("Escuchando... Ctr+C para detener")
+    while True:
+        pass
