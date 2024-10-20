17a8a7bc097f31f549df1ad8c4a779f2a98c8300
ErnestoAragon03 <ernesto.aragon@galileo.edu>
2024-10-20 14:39:07 -0600

    Funciona la transcripción del usuario (ASR)

--- main.py
+++ main.py
@@ -8,37 +8,37 @@ import GUI  #Interfaz gráfica
 #Variable global que indica si se sigue ejecutando main
 running = True
 
-def main():
+def main(app_instance):
     global context, running
-    if asr.conversation_active & GUI.mic_active:
-         #Iniciar el ASR en un hilo separado
-        asr.start_asr_local()   #QUIZÁS REQUIERA DEL USO DE HILOS EN PARALELO
-    else:
-        #Esperar a que se active el microfono ESTO SE TIENE QUE CAMBIAR, MUY INEFICIENTE
-       #while not GUI.mic_active:
-            #NO hacer nada
-            #print("Mic Muted")
-        #Esperar wake word
-        awaked = wake_up.recognize_wake_word()
-        #Iniciar el ASR en un hilo separado
-        if awaked:
-            asr.start_asr_local()
-        
-    if  GUI.mic_active: #Si el microfono estaba activo al momento de llegar
-        if asr.recognized_text:  #Si se ha detectado texto...
-            print(f"Texto detectado: {asr.recognized_text}")
-            #Enciar a LLM
-            llm_response = process_text(asr.recognized_text)
-            #Enviar a TTS
-            process_response(llm_response)
-            asr.recognized_text = ""  #Reiniciar despúes de procesar el texto
+    while running:
+        if asr.conversation_active & GUI.mic_active:
+            #Iniciar el ASR en un hilo separado
+            asr.start_asr_local(app_instance)   #QUIZÁS REQUIERA DEL USO DE HILOS EN PARALELO
         else:
-            asr.conversation_active = False
+            #Esperar a que se active el microfono ESTO SE TIENE QUE CAMBIAR, MUY INEFICIENTE
+        #while not GUI.mic_active:
+                #NO hacer nada
+                #print("Mic Muted")
+            #Esperar wake word
+            awaked = wake_up.recognize_wake_word()
+            #Iniciar el ASR en un hilo separado
+            if awaked:
+                asr.start_asr_local(app_instance)
+            
+        if  GUI.mic_active: #Si el microfono estaba activo al momento de llegar
+            if asr.recognized_text:  #Si se ha detectado texto...
+                print(f"Texto detectado: {asr.recognized_text}")
+                #Enciar a LLM
+                llm_response = process_text(asr.recognized_text)
+                #Enviar a TTS
+                process_response(llm_response)
+                asr.recognized_text = ""  #Reiniciar despúes de procesar el texto
+            else:
+                asr.conversation_active = False
 
-    if running:     #Si el hilo sigue corriendo repetir
-        main()
-    else:
-        print("Terminando Pipeline")
+        else:
+            print("Terminando Pipeline")
+            break
             
 
 def process_text(recognized_text):
@@ -58,14 +58,14 @@ def process_response(llm_response):
     generate_audio(llm_response, tts_model)
     pass
 
-def start_pipeline():
+def start_pipeline(app_instance):
     local_db.init_db()
     global llm_model, llm_tokenizer, tts_model
     #Inicializar el modelo LLM Flant-T5
     llm_model, llm_tokenizer = initialize_llm()
     #Inicializar el modelo TTS 
     tts_model = initialize_tts()
-    main()
+    main(app_instance)
 
 
 if __name__ == "__main__":
