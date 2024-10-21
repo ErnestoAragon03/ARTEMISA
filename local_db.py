6cb4926e76a6ac71fdccc95f6086dc18ea4681e1
ErnestoAragon03 <ernesto.aragon@galileo.edu>
2024-10-20 22:05:07 -0600

    Agregadas las intrucciones para crear tablas de usuarios locales y sesiones activas

--- local_db.py
+++ local_db.py
@@ -4,16 +4,34 @@ def init_db():
     conn = sqlite3.connect('artemisa_local_db')    #Se contecta o crea la base de datos
     cursor = conn.cursor()
 
-    #Crear tablas si no existen
+    #Crear tabla de consultas recientes
     cursor.execute('''CREATE TABLE IF NOT EXISTS recent_consults(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 question TEXT NOT NULL,
                 answer TEXT NOT NULL,
                 timestamp DATATIME DEFAULT CURRENT_TIMESTAMP)''')
+    #Crear tabla de Data importante
     cursor.execute('''CREATE TABLE IF NOT EXISTS important_data(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 key TEXT NOT NULL,
                 value TEXT NOT NULL)''')
+    #Crear tabla de cuentas
+    cursor.execute('''CREATE TABLE IF NOT EXISTS local_users(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   username TEXT NOT NULL,
+                   email TEXT NOT NULL UNIQUE,
+                   password TEXT NOT NULL
+                   session_active INTEGER DEFAULT 0
+                   )
+                    ''')
+         
+    #Crear tabla de sesiones activas
+    cursor.execute('''CREATE TABLE IF NOT EXISTS session(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    user_id INTEGER
+                    FOREIGN KEY(user_id) REFERENCES local_users(id)
+                   )
+                   ''')
     conn.commit()
     conn.close()
 
