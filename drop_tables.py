e79f27e7dbf4586cded07a8a13e967ed4e037887
ErnestoAragon03 <ernesto.aragon@galileo.edu>
2024-10-25 23:27:56 -0600

    Modificado modelo de base de datos (Hubieron recortes)

--- drop_tables.py
+++ drop_tables.py
@@ -16,6 +16,24 @@ def drop_local_users():
     conn.commit()
     conn.close()
 
+def drop_recent_consults():
+    conn = sqlite3.connect('artemisa_local_db')
+    cursor = conn.cursor()
+
+    cursor.execute("DROP TABLE recent_consults")
+    conn.commit()
+    conn.close()
+
+def drop_important_data():
+    conn = sqlite3.connect('artemisa_local_db')
+    cursor = conn.cursor()
+
+    cursor.execute("DROP TABLE important_data")
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
     drop_session()
     drop_local_users()
+    drop_recent_consults()
+    drop_important_data()
--- local_db.py
+++ local_db.py
@@ -4,31 +4,25 @@ def init_db():
     conn = sqlite3.connect('artemisa_local_db')    #Se contecta o crea la base de datos
     cursor = conn.cursor()
 
+     #Crear tabla de cuentas
+    cursor.execute('''CREATE TABLE IF NOT EXISTS local_users(
+                   email TEXT NOT NULL PRIMARY KEY,
+                   username TEXT NOT NULL,
+                   password TEXT NOT NULL,
+                   logged BOOLEAN,
+                   active BOOLEAN)''')
+    
     #Crear tabla de consultas recientes
     cursor.execute('''CREATE TABLE IF NOT EXISTS recent_consults(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT PRIMARY KEY,
                 question TEXT NOT NULL,
                 answer TEXT NOT NULL,
                 timestamp DATATIME DEFAULT CURRENT_TIMESTAMP)''')
-    #Crear tabla de Data importante
-    cursor.execute('''CREATE TABLE IF NOT EXISTS important_data(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                key TEXT NOT NULL,
-                value TEXT NOT NULL)''')
-    #Crear tabla de cuentas
-    cursor.execute('''CREATE TABLE IF NOT EXISTS local_users(
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   username TEXT NOT NULL,
-                   email TEXT NOT NULL UNIQUE,
-                   password TEXT NOT NULL)''')
          
     #Crear tabla de sesiones activas
     cursor.execute('''CREATE TABLE IF NOT EXISTS session(
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    user_email INTEGER,
-                    FOREIGN KEY(user_email) REFERENCES local_users(email)
-                   )
-                   ''')
+                   user_email TEXT PRIMARY KEY,
+                   FOREIGN KEY(user_email) REFERENCES local_users(email))''')
     conn.commit()
     conn.close()
 
@@ -85,13 +79,12 @@ def get_last_active_session():
     conn = sqlite3.connect('artemisa_local_db')
     cursor  = conn.cursor()
 
-    cursor.execute("SELECT user_email FROM session ORDER BY id DESC LIMIT 1")
+    cursor.execute("SELECT email FROM local_users WHERE logged = TRUE")
     result_email = cursor.fetchone()
-
+    cursor.execute("SELECT username FROM local_users WHERE logged = TRUE")
+    result_username = cursor.fetchone()
+    conn.close()
     if result_email:
-        cursor.execute("SELECT username FROM local_users WHERE email=?", (result_email))
-        result_username = cursor.fetchone()
-        conn.close()
         return result_username, result_email    # Retorna el nombre de usuario y el correo
     else:
         return None, None #No hay sesión activa, devuelve None para activar modo guest
@@ -100,12 +93,12 @@ def update_session(email=None):
     conn = sqlite3.connect('artemisa_local_db')
     cursor = conn.cursor()
 
-    #Eliminar la sesión anterior
-    cursor.execute("DELETE FROM session")
+    #Desactivar la sesión anterior
+    cursor.execute("UPDATE local_users SET logged = FALSE WHERE logged = TRUE")
 
     #Insertar la última sesión activa (o modo guest si es None)
     if email:
-        cursor.execute("INSERT INTO session (user_email) VALUES (?)", (email,))
+        cursor.execute("UPDATE local_users SET logged = TRUE WHERE email = (?)", (email,))
 
     conn.commit()
     conn.close()
